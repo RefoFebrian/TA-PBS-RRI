0d5cf10e1e6bb4caf845f3614591331f02ae9618
RakaAgiSaputra <Raka_Agi_Saputra@teknokrat.ac.id>
2025-05-19 09:43:53 +0700

    raka: menambahkan send token to email

---
+++ Backend/app/api/auth/forgot-password/route.ts
@@ -0,0 +1,56 @@
+import { NextRequest } from "next/server";
+import { prisma } from "../../bridgePrisma";
+import { sendPasswordResetEmail } from "../../bridgeMailer";
+import crypto from "crypto";
+import { getResponse } from "../../bridgeResponse"; // Pastikan path sesuai
+
+export const POST = async (req: NextRequest) => {
+  const { email } = await req.json();
+
+  try {
+    // Temukan user berdasarkan email di database
+    const user = await prisma.user.findFirst({
+      where: { email },
+    });
+
+    // Jika user tidak ditemukan
+    if (!user) {
+      return getResponse(1, "Email not found, Please Register", 404, null);
+    }
+
+    // Hapus token lama jika ada
+    await prisma.user.update({
+      where: {
+        email,
+      },
+      data: {
+        resetToken: null,
+        resetTokenExpiry: null,
+      },
+    });
+
+    // Generate token baru
+    const resetToken = crypto.randomBytes(32).toString("hex");
+    const resetTokenExpiry = new Date(Date.now() + 3600000); // 1 jam
+
+    // Simpan token baru
+    await prisma.user.update({
+      where: { id: user.id },
+      data: { resetToken, resetTokenExpiry },
+    });
+
+    // Kirim email
+    await sendPasswordResetEmail(user.email!, resetToken);
+
+    // Sukses
+    return getResponse(
+      0,
+      "Reset password link telah dikirim ke email Anda. Token berlaku selama 1 jam. Cek folder spam jika tidak muncul.",
+      200,
+      null
+    );
+  } catch (err) {
+    console.error("Error:", err);
+    return getResponse(1, "Internal server error", 500, null);
+  }
+};
