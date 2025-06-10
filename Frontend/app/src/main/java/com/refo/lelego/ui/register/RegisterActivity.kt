369da6533165d1d420d9537edc538c8f5ded205f
RefoFebrian <muhammadrefof@gmail.com>
2025-06-10 17:11:48 +0700

    Master: Penambahan customview tv email

--- Frontend/app/src/main/java/com/refo/lelego/ui/register/RegisterActivity.kt
+++ Frontend/app/src/main/java/com/refo/lelego/ui/register/RegisterActivity.kt
@@ -6,13 +6,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.refo.lelego.R
+import com.refo.lelego.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
