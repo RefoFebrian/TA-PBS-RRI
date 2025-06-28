c3ff45cfa0612a3411cf5a207a90e61e09400589
RefoFebrian <muhammadrefof@gmail.com>
2025-06-28 22:21:14 +0700

    Master: penambahan button logout dan all warung response

---
+++ Frontend/app/src/main/java/com/refo/lelego/data/response/AllWarungResponse.kt
@@ -0,0 +1,66 @@
+package com.refo.lelego.data.response
+
+import com.google.gson.annotations.SerializedName
+
+data class AllWarungResponse(
+
+	@field:SerializedName("metadata")
+	val metadata: Metadata? = null,
+
+	@field:SerializedName("data")
+	val data: List<DataItem?>? = null
+)
+
+data class Penjual(
+
+	@field:SerializedName("username")
+	val username: String? = null
+)
+
+data class Metadata(
+
+	@field:SerializedName("error")
+	val error: Int? = null,
+
+	@field:SerializedName("message")
+	val message: String? = null,
+
+	@field:SerializedName("status")
+	val status: Int? = null
+)
+
+data class DataItem(
+
+	@field:SerializedName("jam_buka")
+	val jamBuka: String? = null,
+
+	@field:SerializedName("jam_tutup")
+	val jamTutup: String? = null,
+
+	@field:SerializedName("image")
+	val image: String? = null,
+
+	@field:SerializedName("createdAt")
+	val createdAt: String? = null,
+
+	@field:SerializedName("nama")
+	val nama: String? = null,
+
+	@field:SerializedName("id")
+	val id: Int? = null,
+
+	@field:SerializedName("no_telp")
+	val noTelp: String? = null,
+
+	@field:SerializedName("menu")
+	val menu: List<Any?>? = null,
+
+	@field:SerializedName("penjual")
+	val penjual: Penjual? = null,
+
+	@field:SerializedName("penjualId")
+	val penjualId: String? = null,
+
+	@field:SerializedName("alamat")
+	val alamat: String? = null
+)
