f2765d11be121e56c9a6814ce3f3c6735b5e0e33
RefoFebrian <muhammadrefof@gmail.com>
2025-06-29 22:09:45 +0700

    Master: Fix Build variant

--- Frontend/app/build.gradle.kts
+++ Frontend/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
     buildTypes {
         debug {
             // Gunakan IP 10.0.2.2 untuk emulator, atau IP lokal komputer Anda untuk perangkat fisik
-            buildConfigField("String", "BASE_URL", "\"https://pecel-lele-connect.vercel.app/\"")// sesuaikan dengan base url "network" di terminal saat npm run dev
+            buildConfigField("String", "BASE_URL", "\"http://192.168.1.8:3000\"")// sesuaikan dengan base url "network" di terminal saat npm run dev
         }
         release {
             buildConfigField("String", "BASE_URL", "\"https://pecel-lele-connect.vercel.app/\"")
@@ -30,6 +30,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
